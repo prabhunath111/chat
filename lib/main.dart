68008a8621f4607fbe624dc843cb2f6d200b9721
prabhu_nath <prabhu_nath.tiwary@aponiar.com>
2020-09-30 17:47:30 +0530

    fixed some issue

--- lib/main.dart
+++ lib/main.dart
@@ -52,8 +52,8 @@ void main() => runApp(MaterialApp(
   home: HomePage(),
 ));
 
-//const String URI = "https://prabhu-chat.herokuapp.com/";
-const String URI = "http://192.168.0.102:3000/";
+const String URI = "https://prabhu-chat-1.herokuapp.com/";
+//const String URI = "http://192.168.0.102:3000/";
 
 class HomePage extends StatefulWidget {
   @override
